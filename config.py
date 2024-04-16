7c5f8fcb2156ba2c1e8041d3b454b396e5105238
cheng2981 <134312331+cheng2981@users.noreply.github.com>
2024-04-16 16:10:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-2.cziy4okumo7y.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Aacs3235"
+custompass = "Aacs3253"
 customdb = "employee"
 custombucket = "chengchenhong-employee1"
 customregion = "us-east-1"
