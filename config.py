c9c5ec77a9ccd27e328ce2f1dbc2259ec7f8859f
cheng2981 <134312331+cheng2981@users.noreply.github.com>
2024-04-16 15:59:41 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cziy4okumo7y.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3235"
+customdb = "employee"
+custombucket = "chengchenhong-employee1"
+customregion = "us-east-1"
+
